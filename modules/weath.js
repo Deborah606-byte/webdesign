9bbe8b5e94f963fc7687518533530f0fbfd7f9b8
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-03-31 15:52:32 +0400

    add new changes

--- modules/weath.js
+++ modules/weath.js
@@ -1,19 +1,118 @@
 chartIt(); 
+const filterChart = document.getElementById('filterChart');
+const chartForm = document.getElementById('filterForm');
+
+chartForm.addEventListener("submit, handleFilter");
+
+let myChart;
+
+async function handleFilter(e){
+  myChart.destroy();
+
+  e.preventDefault();
+  let selectedFilter = Number(filterChart,value);
+
+  if(selectedFilter === "") return alert ("Select a value to filter");
+
+  const data = await getData();
+
+  let filteredDataY = [];
+
+  data.ys.filter((value) => {
+    if(selectedFilter == 0){
+      if(Number(value) < 0){
+        filteredDataY.push(value);
+      }
+    }else if(selectedFilter == 4.9){
+      if(Number(value) < 4.9 && Number(value) >= 0){
+          filteredDataY.push(value);
+      }
+    }else if(selectedFilter == 5){
+      if(Number(value) >= 5){
+          filteredDataY.push(value);
+      }
+    }
+  })
+
+  chartIt();
+
+  async function chartIt(){
+    const data = await getData();
+    const ctx = document.getElementById('myChartWealth');
+    
+      myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: data.xs,
+          datasets: [{
+            label: 'Quaterly GDP(Q4 2022)',
+            data: data.ys,
+            backgroundColor: [
+              'rgba(255, 26, 104, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(0, 0, 0, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 26, 104, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(0, 0, 0, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        // options: {
+        //     scales: {
+        //         y: {
+        //             ticks: {
+        //                 // Include a degree sign in the ticks
+        //                 callback: function(value, index, ticks) {
+        //                     return value + '\u00B0';
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+      });
+    }
+}
 
 async function chartIt(){
 const data = await getData();
 const ctx = document.getElementById('myChartWealth');
 
-  new Chart(ctx, {
-    type: 'bar',
+  myChart = new Chart(ctx, {
+    type: 'line',
     data: {
       labels: data.xs,
       datasets: [{
-        label: 'Wealth Distribution',
+        label: 'Quaterly GDP(Q4 2022)',
         data: data.ys,
-		// fill: false,
-		backgroundColor: 'rgba(255, 90, 132, 0.2)',
-		borderColor: 'rgba(255, 90, 132, 1)',
+        backgroundColor: [
+          'rgba(255, 26, 104, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(0, 0, 0, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 26, 104, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(0, 0, 0, 1)'
+        ],
         borderWidth: 1
       }]
     },
@@ -36,7 +135,7 @@ async function getData(){
     const xs = [];
   	const ys = [];
 
-	const response = await fetch('../data/WEALTH_27032023104704851.csv');
+	const response = await fetch('../data/DP_LIVE_31032023125455270.csv');
    
 	const data = await response.text();
 
@@ -45,7 +144,7 @@ async function getData(){
 		const column = row.split(',');
 		const country = column[0];
         xs.push(country);
-		const value = column[8];
+		const value = column[6];
         ys.push(value);
 		console.log(table);
 	});
