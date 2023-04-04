f9a846b6cdf8200f1664124f8e0b0c03a9c48679
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-04-05 01:02:55 +0400

    worked on responsiveness

--- modules/data.js
+++ modules/data.js
@@ -29,33 +29,44 @@ async function getData() {
           data: {
             labels: subRegion,
             datasets: [{
-              barThickness: 30,
+              // barThickness: 10,
               label: "Number of Countries",
               data: subRegionNumber,
               backgroundColor: [
                 "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)"
               ],
               borderColor: [
                 "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)"
               ],
-              borderWidth: 1
+              borderWidth: 2,
+              hoverBackgroundColor: "rgba(255,99,132,0.4)",
+              hoverBorderColor: "rgba(255,99,132,1)",
             }]
           },
+          options:{
+            scales: {
+              y: {
+                stacked: true,
+                grid: {
+                  display: true,
+                  color: "rgba(255,99,132,0.2)"
+                }
+              },
+              x: {
+                grid: {
+                  display: false
+                }
+              }
+          }
+          },
+            
         });
       })
       .catch((e) =>{
         console.log(e)
       })
+
+
   }
   
   
--- modules/menu.js
+++ modules/menu.js
@@ -1,12 +1,16 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
+const discover = document.querySelector('.hero');
 
 burger.addEventListener('click', () =>{
     if(menu.classList.contains('hidden')){
         menu.classList.remove('hidden');
         menu.classList.add('flex');
+        discover.classList.add("discover");
     } else{
         menu.classList.remove('flex');
         menu.classList.add('hidden');
+        discover.classList.remove("discover");
     }
 });
+
