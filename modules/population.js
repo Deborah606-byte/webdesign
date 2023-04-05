e9fc36f3dd3e1dcf55b0302a5f243362d7a62d44
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-04-05 17:00:53 +0400

    worked on the search filter

--- modules/population.js
+++ modules/population.js
@@ -1,23 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", function() {
+  const countryList = document.getElementById('country-info');
+  const listName = document.getElementById('displayList');
 
-    const selectDrop = document.querySelector('#countries');
-    // const selectDrop = document.getElementById('countries');
-  
-  
-    fetch('https://restcountries.com/v3.1/all').then(res => {
-      return res.json();
-    }).then(data => {
-      let output = "";
-      data.forEach(country => {
-        output += `
-        
-        <option value="${country.name}">${country.name}</option>`;
-      })
-  
-      selectDrop.innerHTML = output;
-    }).catch(err => {
-      console.log(err);
-    })
-  
-  
+  function countryListName(list){
+      const countryNames = list.map(country => country.name.common);
+      listName.innerHTML = '';
+      countryNames.forEach(name => {
+        const option = document.createElement('option');
+        option.value = name;
+        option.text = name;
+        listName.add(option);
+      });
+  }
+  countryList.addEventListener("keyup", (e) =>{
+      e.preventDefault();
+
+      displayList.classList.add("hidden");
+      
+      const searchTerm = e.target.value.toLowerCase().trim();
+      
+      const url = 'https://restcountries.com/v3.1/all';
+    
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const filteredData = data.filter(country => {
+          return country.name.common.toLowerCase().startsWith(searchTerm);
+        });
+        countryListName(filteredData);
+      });
   });
+});
