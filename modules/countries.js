52bd6a5784ccda80ffe090d01eaa7de32aa3555f
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-04-06 11:33:54 +0400

    worked on the countries page

--- modules/countries.js
+++ modules/countries.js
@@ -1,24 +1,29 @@
+const search = document.querySelector("#search")
 const countries = document.querySelector(".countries");
+const dropList = document.querySelector(".dropDown");
+
+let countryData = [];
 
 async function getCountry(){
   const url = await fetch("https://restcountries.com/v3.1/all");
   const res = await url.json();
-  console.log(res);
-  showCountry(res)
+  countryData = res;
+  console.log(countryData);
+  showCountry(countryData.slice(0,6));
 }
 
 getCountry();
 
 function showCountry(data){
-  countries.classList.remove('hidden');
-  data.slice(0,6).map((country) => {
+  countries.innerHTML = "";
+  data.forEach((country) => {
     countries.innerHTML += `
       <div class="continent">
         <div class="country-img">
           <img src="${country.flags.png}" alt="country">
         </div>
         <div class="country-info">
-        <h4>${}</h4>
+        <h4>${country.name.common}</h4>
         <p><strong>Population: </strong>${country.population}</p>
         <p><strong>Region: </strong>${country.region}</p>
         <p><strong>Capital: </strong>${country.capital}</p>
@@ -26,5 +31,34 @@ function showCountry(data){
       </div> 
       `;
   });
-
 }
+
+function searchCountry(query){
+  const filteredData = countryData.filter((country) => {
+    const name = country.name.common.toLowerCase();
+    return name.includes(query.toLowerCase());
+  });
+  showCountry(filteredData);
+}
+
+search.addEventListener("input", (event) => {
+  const query = event.target.value;
+  if (query.trim() !== "") {
+    searchCountry(query);
+  } else {
+    showCountry(countryData.slice(0,6));
+  }
+});
+
+dropList.addEventListener("change", (event) => {
+  const selectedContinent = event.target.value;
+  if (selectedContinent === "select continent") {
+    showCountry(countryData.slice(0,6));
+  } else {
+    const filteredData = countryData.filter((country) => {
+      return country.region === selectedContinent;
+    });
+    showCountry(filteredData);
+  }
+});
+
